676770d9634700dc4f4b7888d5b64524b0b054dd
pepab0t <cernikpepa@seznam.cz>
2022-07-03 21:30:25 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from PySide2 import QtWidgets, QtCore, QtGui, QtUiTools
+
+class UiLoader(QtUiTools.QUiLoader):
+    _baseinstance = None
+
+    def createWidget(self, classname, parent=None, name=''):
+        if parent is None and self._baseinstance is not None:
+            widget = self._baseinstance
+        else:
+            widget = super(UiLoader, self).createWidget(classname, parent, name)
+            if self._baseinstance is not None:
+                setattr(self._baseinstance, name, widget)
+        return widget
+
+    def loadUi(self, uifile, baseinstance=None):
+        self._baseinstance = baseinstance
+        widget = self.load(uifile)
+        QtCore.QMetaObject.connectSlotsByName(widget)
+        return widget
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        UiLoader().loadUi('gui.ui', self)
+
+        # self.pushButton.setEnabled(False)
+
+def main():
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
